59665561217388e383fbb9b974d563ae63009b5b
Christian Nkoy <nkoycm@gmail.com>
2023-08-13 12:22:31 -0400

    First commit

---
+++ src/main/java/com/christian/reactspringbootcrudapi/ReactSpringbootCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.christian.reactspringbootcrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringbootCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringbootCrudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/christian/reactspringbootcrudapi/ReactSpringbootCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christian.reactspringbootcrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringbootCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
